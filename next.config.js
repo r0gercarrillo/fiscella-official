10d560bb236dcfa5879676e155d286ad67cc9fc4
r0gercarrillo <rcarrillo0228@gmail.com>
2025-06-22 13:04:28 -0700

    fix: Use serverComponentsExternalPackages for undici

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['undici'],
+  experimental: {
+    serverComponentsExternalPackages: ['undici'],
+  },
 };
 
 module.exports = nextConfig; 
